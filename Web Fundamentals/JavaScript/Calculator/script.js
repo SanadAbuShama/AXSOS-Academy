c9175adb5ea98c2710397f4e5e78b3b93ce7d97d
Sanad Abu Shama <abushamasanad@hotmail.com>
2022-01-16 14:31:04 +0200

    Calculator

---
+++ Web Fundamentals/JavaScript/Calculator/script.js
@@ -0,0 +1,56 @@
+var operation = "";
+var firstNum = "";
+var secondNum = "";
+
+var displayDiv = document.querySelector("#display");
+
+// Set numbers and display
+
+function press(input) {
+  if (operation == "") {
+    firstNum += input;
+    displayDiv.innerText = firstNum;
+  } else {
+    secondNum += input;
+    displayDiv.innerText = secondNum;
+  }
+}
+
+// Set mathematical operation
+
+function setOP(operationInput) {
+  operation = operationInput;
+}
+
+// Clear all values
+
+function clr() {
+  firstNum = "";
+  secondNum = "";
+  operation = "";
+  displayDiv.innerText = 0;
+}
+
+// Calculate the result based on the operation and display it
+
+function calculate() {
+  var result = 0;
+  console.log("executed");
+  if (firstNum != "") {
+    result = firstNum;
+  }
+  if (secondNum != "" && operation != "") {
+    if (operation == "+") {
+      result = parseFloat(firstNum) + parseFloat(secondNum);
+    } else if (operation == "-") {
+      result = parseFloat(firstNum) - parseFloat(secondNum);
+    } else if (operation == "*") {
+      result = parseFloat(firstNum) * parseFloat(secondNum);
+    } else if (operation == "/") {
+      result = parseFloat(firstNum) / parseFloat(secondNum);
+    }
+  }
+  displayDiv.innerText = result;
+  firstNum = result;
+  secondNum = "";
+}
