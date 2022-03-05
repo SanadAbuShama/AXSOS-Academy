527295d43c3eecc1cee7a30b2a6df2cbd59def3d
Sanad Abu Shama <abushamasanad@hotmail.com>
2022-03-05 15:28:20 +0200

    First Java Project

---
+++ Java/Fundamentals/Your_own_java_project/Hello.java
@@ -0,0 +1,8 @@
+
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("My name is Sanad");
+        System.out.println("I am 24 years old");
+        System.out.println("My hometown is Ramallah, Palestine");
+    }
+}
