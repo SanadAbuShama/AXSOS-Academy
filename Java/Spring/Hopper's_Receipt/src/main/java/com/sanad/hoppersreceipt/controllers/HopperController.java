2a05b5b1e4ea7e19a2c4500fb18075168ba15a9e
Sanad Abu Shama <abushamasanad@hotmail.com>
2022-03-12 15:35:25 +0200

    Hopper's Receipt

---
+++ Java/Spring/Hopper's_Receipt/src/main/java/com/sanad/hoppersreceipt/controllers/HopperController.java
@@ -0,0 +1,28 @@
+package com.sanad.hoppersreceipt.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HopperController {
+	// class definition and imports here...
+	@RequestMapping("/")
+	public String index(Model model) {
+
+		String name = "Sanad";
+		String itemName = "Steel wire";
+		double price = 5.25;
+		String description = "Metal strips, also an illustration of nanoseconds.";
+		String vendor = "Little Things Corner Store";
+
+		model.addAttribute("name", name);
+		model.addAttribute("itemName", itemName);
+		model.addAttribute("price", price);
+		model.addAttribute("description", description);
+		model.addAttribute("vendor", vendor);
+		return "index.jsp";
+	}
+	// ...
+
+}
