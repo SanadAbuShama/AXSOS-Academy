26929145eaf597c6885154cd12ff7783d4e42143
Sanad Abu Shama <abushamasanad@hotmail.com>
2022-04-08 17:14:41 +0300

    Putting it together

--- MERN/React/Class_Components/Prop_it_up/prop_it_up/src/components/PersonCard.js
+++ MERN/React/Class_Components/Prop_it_up/prop_it_up/src/components/PersonCard.js
@@ -1,15 +1,30 @@
 import React, { Component } from "react";
 
 class PersonCard extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      age: this.props.age,
+    };
+  }
+
+  clickHandler = () => {
+    const age = this.state.age;
+    this.setState({ age: age + 1 });
+  };
+
   render() {
-    const { firstName, lastName, age, hairColor } = this.props;
+    const { firstName, lastName, hairColor } = this.props;
     return (
       <>
         <h2>
           {lastName}, {firstName}
         </h2>
-        <p>Age: {age}</p>
+        <p>Age: {this.state.age}</p>
         <p>Hair Color: {hairColor}</p>
+        <button onClick={this.clickHandler}>
+          Birthday button for {firstName} {lastName}
+        </button>
       </>
     );
   }
