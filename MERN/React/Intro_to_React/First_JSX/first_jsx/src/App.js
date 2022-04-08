cb3db962ef6ab0fbd322fe7fffc5901350ce7ef7
Sanad Abu Shama <abushamasanad@hotmail.com>
2022-04-08 15:22:29 +0300

    First JSX

---
+++ MERN/React/Intro_to_React/First_JSX/first_jsx/src/App.js
@@ -0,0 +1,24 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <div className="container">
+        <div className="row my-5">
+          <div className="col-4 text-start">
+            <h1>Hello Dojo!</h1>
+            <h4>Things I need to do:</h4>
+            <ul>
+              <li>Learn React</li>
+              <li>Climb Mt. Everest</li>
+              <li>Run a Marathon</li>
+              <li>Feed the dogs</li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
