cc7d651826c1c63aaa2373f566dbe4661fd0a6ac
Sanad Abu Shama <abushamasanad@hotmail.com>
2022-04-11 14:11:59 +0300

    Pokemon API

---
+++ MERN/React/APIs/Pokemon_API/pokemon_api/src/components/Pokemons.js
@@ -0,0 +1,29 @@
+import React, { useState } from "react";
+
+const Pokemons = () => {
+  const [pokemons, setPokemons] = useState([]);
+
+  const getPokemons = () => {
+    fetch("https://pokeapi.co/api/v2/pokemon?limit=100000&offset=0")
+      .then((response) => response.json())
+      .then((response) => setPokemons(response.results));
+  };
+  return (
+    <div className="container my-5">
+      <div className="row justify-content-center">
+        <div className="col-6 offset-3 text-start">
+          <button className="btn btn-secondary mb-5" onClick={getPokemons}>
+            Fetch Pokemons
+          </button>
+          <ul>
+            {pokemons
+              ? pokemons.map((pokemon, i) => <li key={i}>{pokemon.name}</li>)
+              : ""}
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Pokemons;
