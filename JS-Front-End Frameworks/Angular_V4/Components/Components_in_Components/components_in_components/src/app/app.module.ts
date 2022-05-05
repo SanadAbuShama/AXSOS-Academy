438193d0b3b9f205364c509db87c53f532061cac
Sanad Abu Shama <abushamasanad@hotmail.com>
2022-05-05 16:55:55 +0300

    Components in Components

---
+++ JS-Front-End Frameworks/Angular_V4/Components/Components_in_Components/components_in_components/src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppComponent } from './app.component';
+import { TasksComponent } from './tasks/tasks.component';
+import { NotesComponent } from './notes/notes.component';
+import { TimeComponent } from './time/time.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    TasksComponent,
+    NotesComponent,
+    TimeComponent
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
